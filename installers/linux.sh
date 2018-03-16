84ec05a517183bffd75c5ce31074f4a04677d9b2
Tom McKeesick <tmck01@gmail.com>
2018-03-17 05:02:30 +1100

    Remove unused installs

--- installers/linux.sh
+++ installers/linux.sh
@@ -2,26 +2,21 @@
 
 set -euxo pipefail
 
+cd $HOME
+
 echo '> Running OS updates and upgrades'
 sudo apt update
-sudo apt upgrade -y
 
 echo '> Installing basic dependencies'
 sudo apt install -y \
     curl \
     cowsay fortune \
     tmux \
-    docker docker-compose \
+    docker \
     git checkinstall locales
 
-if [ ! -d $HOME/pokemonsay ]; then
-    cd $HOME
+if [ ! -f $HOME/bin/pokemonsay ]; then
     git clone http://github.com/possatti/pokemonsay
-    cd pokemonsay
-    ./install.sh
-    cd
+    (cd pokemonsay && ./install.sh)
 fi
 
-# echo '> Setting locale & timezone'
-# # echo "Australia/Melbourne" > /etc/timezone
-# sudo dpkg-reconfigure -f noninteractive tzdata
