3701a3212301d3a8cffac0c942ccdf0056f9656a
Tom McKeesick <tmck01@gmail.com>
2018-03-17 08:16:27 +1100

    Style tweaks

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -2,9 +2,12 @@
 
 set -euxo pipefail
 
+dvd_drive=true
+multimedia=true
+
 # Update, remove crap & upgrade
-sudo apt update
 sudo apt remove -y libreoffice* thunderbird* rhythmbox*
+sudo apt update
 sudo apt upgrade -y
 
 # Update to newest version of distro (if available)
@@ -12,21 +15,27 @@ sudo do-release-upgrade -f DistUpgradeViewNonInteractive
 
 # Basic deps
 sudo apt install -y \
-    cowsay fortune tmux \
-    docker docker-compose \
-    wget git checkinstall locales build-essential yasm \
-    htop iotop bmon nethogs \
-    ilc handbrake guake redshift
+  wget curl git \
+  tmux htop iotop bmon nethogs \
+  cowsay fortune \
+  checkinstall locales build-essential yasm \
+  docker \
+  handbrake guake redshift
 
 # Bypass DVD copy protection
-sudo DEBIAN_FRONTEND=noninteractive apt -yq install libdvd-pkg
+if $dvd_drive; then
+  sudo DEBIAN_FRONTEND=noninteractive apt -yq install libdvd-pkg
+fi
 
 # ffmpeg
-sudo apt install -y libass-dev libfreetype6-dev \
-  libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
-  libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev \
-  libmp3lame-dev libfdk-aac-dev libx264-dev libx265-dev
-sudo apt install -y ffmpeg
+if $multimedia; then
+  sudo apt install -y \
+    libass-dev libfreetype6-dev \
+    libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+    libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev \
+    libmp3lame-dev libfdk-aac-dev libx264-dev libx265-dev
+  sudo apt install -y ffmpeg
+fi
 
 # Clean up
 sudo apt autoremove -y
