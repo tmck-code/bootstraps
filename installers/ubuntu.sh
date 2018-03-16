1c1bdd0df42d72b2ffbc69dbe61c5012785e4527
Tom McKeesick <tmck01@gmail.com>
2018-03-17 05:03:02 +1100

    Tidy up & use apt instead of apt-get

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -3,30 +3,31 @@
 set -euxo pipefail
 
 # Update, remove crap & upgrade
-sudo apt-get update
-sudo apt-get remove -y libreoffice* thunderbird* firefox* rhythmbox*
-sudo apt-get upgrade -y
+sudo apt update
+sudo apt remove -y libreoffice* thunderbird* rhythmbox*
+sudo apt upgrade -y
+
+# Update to newest version of distro (if available)
+sudo do-release-upgrade -f DistUpgradeViewNonInteractive
 
 # Basic deps
-sudo apt-get install -y \
+sudo apt install -y \
     cowsay fortune tmux \
     docker docker-compose \
     wget git checkinstall locales build-essential yasm \
     htop iotop bmon nethogs \
-    vlc handbrake guake terminator redshift
+    ilc handbrake guake redshift
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install libdvd-pkg
-
-# Plex media server
-# echo deb https://downloads.plex.tv/repo/deb ./public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
-# curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
-# sudo apt-get update && sudo apt-get install -y plexmediaserver
+# Bypass DVD copy protection
+sudo DEBIAN_FRONTEND=noninteractive apt -yq install libdvd-pkg
 
 # ffmpeg
-sudo apt-get install -y libass-dev libfreetype6-dev \
+sudo apt install -y libass-dev libfreetype6-dev \
   libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
   libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev \
   libmp3lame-dev libfdk-aac-dev libx264-dev libx265-dev
-sudo apt-get install -y ffmpeg
+sudo apt install -y ffmpeg
 
-sudo apt-get autoremove -y
+# Clean up
+sudo apt autoremove -y
+
