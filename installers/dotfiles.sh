77e96f7a6a82d7c3ae5041611d3930183df649fb
Tom McKeesick <tmck01@gmail.com>
2018-03-31 13:54:38 +1100

    Use the deploy script in the dotfiles repo

--- installers/dotfiles.sh
+++ installers/dotfiles.sh
@@ -10,14 +10,5 @@ else
   git clone https://github.com/tmck-code/dotfiles.git
 fi
 
-cd dotfiles
-mkdir -p ~/bin/
-
-cp lean/bash_aliases ~/.bash_aliases
-cp lean/bin/*        ~/bin/
-
-cp bashrc       ~/.bashrc
-cp vimrc        ~/.vimrc
-cp gitconfig    ~/.gitconfig
-cp tmux_conf    ~/.tmux.conf
+(cd dotfiles && ./deploy.sh)
 
