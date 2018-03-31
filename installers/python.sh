325057a9403830226150bb86861f3bfaf9b9aef8
Tom McKeesick <tmck01@gmail.com>
2018-03-31 14:04:16 +1100

    Install 3.6.4 for now

--- installers/python.sh
+++ installers/python.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 
 # rm -rf $HOME/.pyenv
 
+PYTHON_VERSION=3.6.4
+
 # Install some dependencies
 sudo apt-get install -y \
   libreadline-dev \
@@ -12,14 +14,6 @@ sudo apt-get install -y \
   libsqlite3-dev
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-
-echo '# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-' >> ~/.bash_profile
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
