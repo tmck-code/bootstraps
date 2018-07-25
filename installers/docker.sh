a60bd3184da4d11034a99b6e33ecbed9333e1719
Tom McKeesick <tmck01@gmail.com>
2018-07-25 12:00:14 +1000

    Make debian the default distro for docker install

--- installers/docker.sh
+++ installers/docker.sh
@@ -8,7 +8,7 @@ sudo apt install -y \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
