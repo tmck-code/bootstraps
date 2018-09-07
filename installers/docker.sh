cd234ff20bbc8e7b87abe186e76d56aadbb39f44
Tom McKeesick <tmck01@gmail.com>
2018-09-07 19:46:33 +1000

    Use variable release

--- installers/docker.sh
+++ installers/docker.sh
@@ -8,9 +8,9 @@ sudo apt install -y \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/$(lsb_release -is)/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is) \
    $(lsb_release -cs) \
    stable"
 
