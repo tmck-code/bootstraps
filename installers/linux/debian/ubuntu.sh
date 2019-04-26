c5a6ddf46a5c44eccb4b15c8f3cde494114150cc
Tom McKeesick <tmck01@gmail.com>
2019-04-27 01:20:46 +1000

    Remove guake and disable libdvdcss

--- installers/linux/debian/ubuntu.sh
+++ installers/linux/debian/ubuntu.sh
@@ -13,14 +13,14 @@ sudo apt install -y \
   tmux htop iotop bmon nethogs \
   cowsay fortune \
   checkinstall locales build-essential yasm \
-  handbrake guake redshift
+  handbrake redshift
 
 # Update to newest version of distro (if available)
 # sudo sed -i 's/^Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
 # sudo do-release-upgrade -f DistUpgradeTextView || echo "no new releases"
 
 # Bypass DVD copy protection
-sudo DEBIAN_FRONTEND=noninteractive apt -yq install libdvd-pkg
+# sudo DEBIAN_FRONTEND=noninteractive apt -yq install libdvd-pkg
 
 # Clean up
 sudo apt autoremove -y
