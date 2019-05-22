98829cbf05392478472a21183d6870df66a8a378
freman <freman@tablet-debian.arrakis>
2019-05-22 20:43:48 +1000

    Use debian in debian docker ce install

--- installers/linux/debian/docker.sh
+++ installers/linux/debian/docker.sh
@@ -8,9 +8,9 @@ sudo apt install -y \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 
