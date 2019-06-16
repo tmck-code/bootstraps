9c0efd1781e635a67cf8cf9b8c3ec0adc7a3bb6d
Tom McKeesick <tmck01@gmail.com>
2019-06-16 19:58:28 +1000

    use ssh for git clone

--- installers/dotfiles.sh
+++ installers/dotfiles.sh
@@ -14,7 +14,7 @@ mkdir -p ~/dev && cd ~/dev
 if [ -d dotfiles ]; then
   (cd dotfiles && git pull)
 else
-  git clone https://github.com/tmck-code/dotfiles.git
+  git clone git@github.com:tmck-code/dotfiles.git
 fi
 
 (cd dotfiles && ./deploy.sh "${OS}")
