aae4c535b3cd7d4fe5d02c1493181667de6ed0b2
Tom McKeesick <tmck01@gmail.com>
2019-12-02 22:14:42 +1100

    Update ubuntu installer and remove tools.sh

--- installers/linux/debian/tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Networking ----------------------------------------------
-sudo apt-get install -y \
-  bmon nethogs
-
-# System resources ----------------------------------------
-sudo apt-get install -y \
-  htop iotop
--- installers/linux/debian/ubuntu.sh
+++ installers/linux/debian/ubuntu.sh
@@ -4,24 +4,26 @@ set -euxo pipefail
 
 # Update, remove crap & upgrade
 sudo apt update
-sudo apt remove -y libreoffice* thunderbird* rhythmbox* shotwell* && sudo apt autoremove -y
-sudo apt upgrade -y
+sudo apt purge -y libreoffice* thunderbird* rhythmbox* shotwell*
+sudo apt autoremove -y
+sudo apt upgrade -y --no-install-recommends
 
 # Basic deps
-sudo apt install -y \
+sudo apt install -y --no-install-recommends \
   wget curl git \
   tmux htop iotop bmon nethogs \
   cowsay fortune \
   checkinstall locales build-essential yasm \
   handbrake redshift
 
+# Clean up
+sudo apt autoremove -y
+
+# TODO: verify if this still works
+#
 # Update to newest version of distro (if available)
 # sudo sed -i 's/^Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
 # sudo do-release-upgrade -f DistUpgradeTextView || echo "no new releases"
 
 # Bypass DVD copy protection
 # sudo DEBIAN_FRONTEND=noninteractive apt -yq install libdvd-pkg
-
-# Clean up
-sudo apt autoremove -y
-
