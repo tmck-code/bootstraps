18fd7b4d85a7f810b85cac7461a7cfa4c79c3501
Tom McKeesick <tmck01@gmail.com>
2020-05-14 22:39:33 +1000

    Update and consolidate debian bootstrapper

--- installers/linux/debian.sh
+++ installers/linux/debian.sh
@@ -1,30 +1,74 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
-function install_packages() {
-  # Update & upgrade
-  sudo apt update
-  sudo apt remove -y libreoffice* thunderbird* rhythmbox* shotwell*
+# Remove the largest, least-used programs. Saves a lot of time when running 
+# `apt-get upgrade` for the first time
+function purge_bloat() {
+  echo "> Purging largest programs that aren't often used"
+  sudo apt-get update
+  sudo apt-get purge -y libreoffice* thunderbird* rhythmbox* shotwell*
   sudo apt-get autoremove -y
-  sudo apt upgrade -y --no-install-recommends
+}
 
-  # Basic deps
-  sudo apt install -y --no-install-recommends \
+# Removes bloaty programs and then upgrades the system.
+function clean_slate() {
+  purge_bloat
+
+  echo "> Upgrading all OS packages"
+  sudo apt-get update
+  sudo apt-get upgrade -y
+}
+
+# Install base dependencies
+function install_base() {
+  echo "> Installing base OS packages"
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends \
     wget curl git tig tree \
-    tmux htop iotop bmon nethogs sysstat \
+    tmux htop iotop bmon sysstat \
+    parallel \
     cowsay fortune \
     locales build-essential yasm \
+    brasero \
     redshift \
     software-properties-common
+}
 
-  # Clean up
-  sudo apt-get autoremove -y
+
+# Install tools used in .bashrc
+# - my pokesay lib
+# - "high-performance" lolcat
+function install_pokesay() {
+  echo "> Installing pokesay tools for .bashrc"
+  cd /usr/local/src
+
+  sudo apt-get update
+  sudo apt-get install -y cowsay fortune
+
+  if [ ! -f $HOME/bin/pokesay ]; then
+    git clone --depth 1 http://github.com/tmck-code/pokesay
+    (cd pokesay && ./install.sh)
+  fi
+
+  if [ ! -f $HOME/bin/lolcat ]; then
+    git clone --depth 1 https://github.com/jaseg/lolcat.git
+    (cd lolcat && make lolcat && cp ./lolcat $HOME/bin/)
+  fi
+}
+
+function bootstrap() {
+  echo "> Bootstrapping debian"
+  clean_slate
+  install_base
+  install_pokesay
+  echo "> Bootstrap complete!"
 }
 
 
 case ${1:-} in
-  "packages" ) install_packages ;;
-  *)           install_packages ;;
+  "base" )        install_base ;;
+  "pokesay" )     install_pokesay ;;
+  "clean_slate" ) clean_slate ;;
+  "bootstrap"|* )   bootstrap ;;
 esac
-
