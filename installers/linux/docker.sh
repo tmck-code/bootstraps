7764be37a8986524be0b6c2ad00e68ea60d639d6
Tom McKeesick <tmck01@gmail.com>
2020-06-04 11:48:34 +1000

    adjust docker install for bullseye

--- installers/linux/docker.sh
+++ installers/linux/docker.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-sudo apt purge -y docker docker-engine docker.io
 sudo apt update
+sudo apt purge -y docker docker-engine docker.io
 sudo apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
+release=$(lsb_release -cs)
+
+if [ "${release}" == "bullseye" ]; then
+  release="buster"
+  echo "- Using debian release 'buster' instead of 'bullseye'!"
+else
+  echo "- Using debian release '${release}'!"
+fi
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
+   ${release} \
    stable"
 
 sudo apt update
