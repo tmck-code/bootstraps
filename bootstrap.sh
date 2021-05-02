14fc5dde84df5e20ff3c6b79b038411d2c61589d
Tom McKeesick <tmck01@gmail.com>
2021-05-02 14:53:47 +1000

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-sudo apt update && \
-  sudo apt install -y git && \
-  cd && \
-  mkdir -p dev && \
-  cd dev && \
-  git clone https://github.com/tmck-code/bootstraps.git && \
-  cd bootstraps
+set -euo pipefail
+
+sudo apt update
+sudo apt install -y git
+
+cd
+mkdir -p dev
+cd dev
+git clone https://github.com/tmck-code/bootstraps.git
+cd bootstraps
 
 ./installers/dotfiles.sh
+./installers/linux/debian.sh
+./installers/linux/docker.sh
+./installers/linux/vim_full.sh
+./installers/linux/ffmpeg_compile.sh
