5bd40d244442667fab6158dccc4bfa3683e128ac
Tom McKeesick <tmck01@gmail.com>
2021-06-11 22:08:50 +1000

    Update termux bootstrapper

--- installers/linux/termux.sh
+++ installers/linux/termux.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+[ -n "${DEBUG:-}" ] && set -x
+
 function update() {
   echo "- Updating package lists and upgrading"
   pkg update
@@ -19,12 +21,32 @@ function install_dependencies() {
 function install_essentials() {
   echo "- Installing essential tools"
   pkg install -y \
-    git tig vim man \
+    tree coreutils openssh build-essential \
+    git tig vim man shellcheck \
+    python3 golang \
     htop tmux bmon \
     termux-api termux-tools
 }
 
+function install_lolcat() {
+  echo "- Installing lolcat"
+  cd "${PREFIX}/tmp"
+  git clone git@github.com:jaseg/lolcat.git
+  (cd lolcat && make lolcat)
+  cp lolcat/lolcat "$HOME/bin"
+  rm -rf lolcat
+}
+
 # Install minimum dependencies for dotfiles and bootstrapping to work
-update
-install_dependencies
-install_essentials
+function bootstrap() {
+  update
+  install_dependencies
+  install_essentials
+}
+
+case ${1:-} in
+  "lolcat" )      install_lolcat ;;
+  "bootstrap"|* ) bootstrap ;;
+esac
+
+[ -n "${DEBUG:-}" ] && set +x
