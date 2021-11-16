f3d80a33445bf25fe7955539b95374cfd5bc478d
Tom McKeesick <tmck01@gmail.com>
2021-11-16 23:45:25 +1100

    update ffmpeg cuda

--- installers/linux/ffmpeg_compile.sh
+++ installers/linux/ffmpeg_compile.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+CUDA_OS=ubuntu2004
 CUDA_VERSION_MAJOR=11.5
 CUDA_VERSION=11.5.0
 NVIDIA_DRIVER_VERSION=495.29.05
@@ -25,7 +26,7 @@ function pkg_deps() {
     libunistring-dev
 }
 
-function younger_than_a_week() {
+function older_than_a_week() {
   return 1
   if [ ! -d $1 ]; then
     echo "- $1 does not exist, installing"
@@ -33,22 +34,17 @@ function younger_than_a_week() {
   fi
   age=$[ $[ $(date +%s) - $(date -r "${1}" +%s) ] / 60 / 60 / 24 ]
 
-  if (( "$age" < 7 )); then
-    echo "- $1 is $age days old, skipping"
-    return 1
-  else
-    echo "- $1 is $age days old, updating"
-    return 0
-  fi
+  echo "- $1 is $age days old"
+  (( "$age" > 7 )) && return 0 || return 1
 }
 
 
 function nasm() {
   cd ~/ffmpeg_sources
-  younger_than_a_week nasm-$NASM_VERSION && return
-
-  wget https://www.nasm.us/pub/nasm/releasebuilds/$NASM_VERSION/nasm-$NASM_VERSION.tar.bz2
-  tar xjvf nasm-$NASM_VERSION.tar.bz2
+  if $(older_than_a_week nasm-$NASM_VERSION); then
+    wget https://www.nasm.us/pub/nasm/releasebuilds/$NASM_VERSION/nasm-$NASM_VERSION.tar.bz2
+    tar xjvf nasm-$NASM_VERSION.tar.bz2
+  fi
   cd nasm-$NASM_VERSION
   ./autogen.sh
   ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
@@ -57,24 +53,30 @@ function nasm() {
 }
 
 # NV headers & CUDA
+# Sourced from the excellent official guide: https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
+# and also from the official download site for the CUDA toolkit:
+# ubuntu - https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_network
+# debian - https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Debian&target_version=11&target_type=deb_network
+# This bootstrapper uses the "network" deb installer method, as it can keep up
+# to date automatically with any updates to CUDA packages
 function nv_deps() {
   cd ~/ffmpeg_sources
-  younger_than_a_week nv-codec-headers && return
-
-  git -C nv-codec-headers pull 2> /dev/null || git clone --depth 1 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+  if $(older_than_a_week nv-codec-headers); then
+    git -C nv-codec-headers pull 2> /dev/null || git clone --depth 1 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+  fi
   cd nv-codec-headers
   make
   sudo make -j "$(nproc)" install
 
   # CUDA
-  if [ -d /var/cuda-repo-$CUDA_OS_VERSION_SLUG-local/ ]; then
+  if [[ $(sudo apt show cuda 2> /dev/null) ]]; then
     sudo apt update
     sudo apt upgrade -y
   else
-    wget https://developer.download.nvidia.com/compute/cuda/$CUDA_VERSION/local_installers/cuda-repo-$CUDA_OS_VERSION_SLUG-local_$CUDA_VERSION-$NVIDIA_DRIVER_VERSION-1_amd64.deb
-    sudo dpkg -i cuda-repo-$CUDA_OS_VERSION_SLUG-local_$CUDA_VERSION-$NVIDIA_DRIVER_VERSION-1_amd64.deb
-    sudo apt-key add /var/cuda-repo-$CUDA_OS_VERSION_SLUG-local/7fa2af80.pub
-    # sudo add-apt-repository contrib
+    wget https://developer.download.nvidia.com/compute/cuda/repos/$CUDA_OS/x86_64/cuda-$CUDA_OS.pin
+    sudo mv cuda-$CUDA_OS.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$CUDA_OS/x86_64/7fa2af80.pub
+    sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/$CUDA_OS/x86_64/ /"
     sudo apt-get update
     sudo apt-get -y install cuda
   fi
@@ -82,9 +84,9 @@ function nv_deps() {
 
 function x264() {
   cd ~/ffmpeg_sources
-  younger_than_a_week x264 && return
-
-  git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git
+  if $(older_than_a_week x264); then
+    git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git
+  fi
   cd x264
    ./configure \
     --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic
@@ -94,11 +96,10 @@ function x264() {
 
 function x265() {
   cd ~/ffmpeg_sources
-  younger_than_a_week x265_git && return
-
   sudo apt-get install libnuma-dev
-  git -C x265_git pull 2> /dev/null || git clone https://bitbucket.org/multicoreware/x265_git
-  # if younger_than_a_week x265; then return;  fi
+  if $(older_than_a_week x265_git); then
+    git -C x265_git pull 2> /dev/null || git clone https://bitbucket.org/multicoreware/x265_git
+  fi
   cd x265_git/build/linux
   cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off ../../source
   make -j "$(nproc)"
@@ -107,9 +108,9 @@ function x265() {
 
 function libfdk_aac() {
   cd ~/ffmpeg_sources
-  younger_than_a_week fdk-aac && return
-
-  git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac
+  if $(older_than_a_week fdk-aac); then
+    git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac
+  fi
   cd fdk-aac
   autoreconf -fiv
   ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
@@ -119,9 +120,9 @@ function libfdk_aac() {
 
 function libopus() {
   cd ~/ffmpeg_sources
-  younger_than_a_week opus && return
-
-  git -C opus pull 2> /dev/null || git clone --depth 1 https://github.com/xiph/opus.git
+  if $(older_than_a_week opus); then
+    git -C opus pull 2> /dev/null || git clone --depth 1 https://github.com/xiph/opus.git
+  fi
   cd opus
   ./autogen.sh
   ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
@@ -131,9 +132,9 @@ function libopus() {
 
 function libaom() {
   cd ~/ffmpeg_sources
-  younger_than_a_week aom_build && return
-
-  git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom
+  if $(older_than_a_week aom_build); then
+    git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom
+  fi
   mkdir -p aom_build
   cd aom_build
   cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_TESTS=OFF -DENABLE_NASM=on ../aom
@@ -143,9 +144,9 @@ function libaom() {
 
 function libsvtav1() {
   cd ~/ffmpeg_sources
-  younger_than_a_week SVT-AV1 && return
-
-  git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git
+  if $(older_than_a_week SVT-AV1); then
+    git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git
+  fi
   mkdir -p SVT-AV1/build
   cd SVT-AV1/build
   cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF ..
@@ -155,12 +156,10 @@ function libsvtav1() {
 
 function libdav1d() {
   pip3 install --user meson
-
   cd ~/ffmpeg_sources
-  younger_than_a_week dav1d && return
-
-
-  git -C dav1d pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/dav1d.git
+  if $(older_than_a_week dav1d); then
+    git -C dav1d pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/dav1d.git
+  fi
   mkdir -p dav1d/build
   cd dav1d/build
   meson setup -Denable_tools=false -Denable_tests=false --default-library=static .. --prefix "$HOME/ffmpeg_build" --libdir="$HOME/ffmpeg_build/lib"
@@ -170,10 +169,10 @@ function libdav1d() {
 
 function libvmaf() {
   cd ~/ffmpeg_sources
-  younger_than_a_week vmaf-2.1.1 && return
-
-  wget https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz
-  tar xvf v2.1.1.tar.gz
+  if $(older_than_a_week vmaf-2.1.1); then
+    wget https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz
+    tar xvf v2.1.1.tar.gz
+  fi
   mkdir -p vmaf-2.1.1/libvmaf/build
   cd vmaf-2.1.1/libvmaf/build
   meson setup -Denable_tests=false -Denable_docs=false --buildtype=release --default-library=static .. --prefix "$HOME/ffmpeg_build" --bindir="$HOME/ffmpeg_build/bin" --libdir="$HOME/ffmpeg_build/lib"
@@ -183,10 +182,11 @@ function libvmaf() {
 
 function ffmpeg() {
   cd ~/ffmpeg_sources
-  younger_than_a_week ffmpeg && return
+  if ! $(older_than_a_week ffmpeg); then
+    wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+    tar xjvf ffmpeg-snapshot.tar.bz2
+  fi
 
-  wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
-  tar xjvf ffmpeg-snapshot.tar.bz2
   cd ffmpeg
   # make distclean
   ./configure \
@@ -231,6 +231,9 @@ function remove_compilations() {
 function purge() {
   rm -rf ~/ffmpeg_build ~/ffmpeg_sources ~/bin/{ffmpeg,ffprobe,ffplay,x264,x265,nasm,vsyasm,yasm,ytasm}
   hash -r
+  sudo apt purge -y "*cuda*" "*cublas*" "*cufft*" "*cufile*" "*curand*" "*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "nsight*"
+  sudo apt purge -y "*nvidia*"
+  sudo apt autoremove --purge -y
 }
 
 function install() {
