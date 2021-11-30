00437210d910ec082963a0ab6fd98452d5a7686f
Tom McKeesick <tmck01@gmail.com>
2021-11-30 23:04:11 +1100

    update

--- installers/linux/manjaro.sh
+++ installers/linux/manjaro.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm \
-    git \
-    obs-studio \
-    vim \
-    python3 python-pip \
-    alacritty \
-    tmux cowsay fortune-mod screenfetch \
-    ffmpeg flatpak
+set -euo pipefail
 
+# Base dependencies
+sudo pacman -Syu --noconfirm \
+    git vim ffmpeg \
+    tmux cowsay lolcat fortune-mod screenfetch \
+    pkgconf flatpak \
+    python3 python-pip \
+    obs-studio alacritty
+
+# Steam
 flatpak install flathub com.valvesoftware.Steam
-flatpak run com.valvesoftware.Steam
+# Need this to be able to access another drive
+flatpak override --user --filesystem=/mnt/X/ com.valvesoftware.Steam
+# flatpak run com.valvesoftware.Steam
+
+# pokesay-go
+bash -c "$(curl https://raw.githubusercontent.com/tmck-code/pokesay-go/master/scripts/install.sh)" bash linux amd64
