51aa980b2fb8652e05ffd3442f6ba0415cebab24
Tom McKeesick <tmck01@gmail.com>
2021-11-30 23:41:24 +1100

    update

--- installers/linux/manjaro.sh
+++ installers/linux/manjaro.sh
@@ -2,19 +2,25 @@
 
 set -euo pipefail
 
+# Makes the package manager downloads 100x faster
+sudo pacman-mirrors --country Australia
+
 # Base dependencies
 sudo pacman -Syu --noconfirm \
-    git vim ffmpeg \
-    tmux cowsay lolcat fortune-mod screenfetch \
+    git vim ffmpeg code docker docker-compose \
+    tmux cowsay lolcat fortune-mod screenfetch redshift \
     pkgconf flatpak \
     python3 python-pip \
-    obs-studio alacritty
+    obs-studio alacritty piper
 
-# Steam
-flatpak install flathub com.valvesoftware.Steam
-# Need this to be able to access another drive
-flatpak override --user --filesystem=/mnt/X/ com.valvesoftware.Steam
-# flatpak run com.valvesoftware.Steam
+# Add user to the docker group
+sudo usermod -aG docker $USER
 
 # pokesay-go
 bash -c "$(curl https://raw.githubusercontent.com/tmck-code/pokesay-go/master/scripts/install.sh)" bash linux amd64
+
+# Steam
+# run with: flatpak run com.valvesoftware.Steam
+flatpak install flathub com.valvesoftware.Steam
+# Need this to be able to access another drive
+flatpak override --user --filesystem=/mnt/X/ com.valvesoftware.Steam
