ff1c267a0e7a6b705be10efb455dd2754772e345
Tom McKeesick <tmck01@gmail.com>
2022-04-13 22:24:56 +1000

    minecraft bootstrapper

--- installers/linux/minecraft.sh
+++ installers/linux/minecraft.sh
@@ -2,11 +2,24 @@
 
 set -euo pipefail
 
-wget "https://launcher.mojang.com/download/Minecraft.deb"
 
-sudo apt update
-sudo apt install -y openjdk-11-jre openjdk-11-jre-headless
+function install_launcher() {
+  wget "https://launcher.mojang.com/download/Minecraft.deb"
+  sudo apt update
+  sudo apt install -y openjdk-11-jre openjdk-11-jre-headless
 
-sudo dpkg -i Minecraft.deb
-sudo apt install -f
-rm Minecraft.deb
+  sudo dpkg -i Minecraft.deb
+  sudo apt install -f
+  rm Minecraft.deb
+}
+
+# function install_optifine() { }
+
+function install_fabric() {
+  wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar
+  java -jar fabric-installer-0.10.2.jar
+}
+
+for i in "${@}"; do
+  install_${i}
+done
