a1d5fa9638424e65491147a195a41955903eddf9
Tom McKeesick <tmck01@gmail.com>
2022-05-20 08:49:20 +1000

    update

--- installers/linux/debian.sh
+++ installers/linux/debian.sh
@@ -192,6 +192,20 @@ function install_obs() {
   sudo make install -j "$(nproc)"
 }
 
+function install_brave() {
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+  sudo apt install -y brave-browser
+}
+
+function install_zoom() {
+  cd /tmp/
+  wget https://zoom.us/client/latest/zoom_amd64.deb
+  sudo dpkg -i zoom_amd64.deb
+  rm zoom_amd64.deb
+}
+
 # My favourite shell... I think?
 function install_fish() {
   echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' \
