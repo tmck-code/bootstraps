a4d37bc8f1d6c9b95992a98fcbba6c7f6a89401c
Tom McKeesick <tmck01@gmail.com>
2022-05-19 21:19:36 +1000

    update minecraft bootstrapper

--- installers/linux/minecraft.sh
+++ installers/linux/minecraft.sh
@@ -2,8 +2,20 @@
 
 set -euo pipefail
 
+function install_launcher_arch() {
+  rm -rf /tmp/minecraft-launcher
+  sudo pacman -S jre-openjdk jre-openjdk-headless
 
-function install_launcher() {
+  git clone https://aur.archlinux.org/minecraft-launcher.git /tmp/minecraft-launcher
+  cd /tmp/minecraft-launcher
+
+  makepkg
+  sudo pacman -U minecraft-launcher-*x86_64.pkg.tar.zst
+
+  rm -rf /tmp/minecraft-launcher
+}
+
+function install_launcher_debian() {
   wget "https://launcher.mojang.com/download/Minecraft.deb"
   sudo apt update
   sudo apt install -y openjdk-11-jre openjdk-11-jre-headless
@@ -16,8 +28,11 @@ function install_launcher() {
 # function install_optifine() { }
 
 function install_fabric() {
+  cd /tmp
+  rm -rf fabric-installer*
   wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar
   java -jar fabric-installer-0.10.2.jar
+  rm -rf fabric-installer*
 }
 
 for i in "${@}"; do
