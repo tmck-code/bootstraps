337198a19ed9cd8198a8d50f1a26765d6d3429e8
Tom McKeesick <tmck01@gmail.com>
2023-04-06 00:05:35 +1000

    add github copilot installer

---
+++ installers/linux/copilot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "- installing for vim"
+git clone https://github.com/github/copilot.vim.git \
+  ~/.vim/pack/github/start/copilot.vim
+
+echo "- installing for neovim"
+git clone https://github.com/github/copilot.vim.git \
+  ~/.config/nvim/pack/github/start/copilot.vim
+
+echo "- installing for vscode"
+code --install-extension GitHub.copilot
+
+echo "! run :Copilot setup & :Copilot enable when in vim/nvim"
