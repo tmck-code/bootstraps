59b535f2739db301fe20f27542a054ff27e4d78e
Tom McKeesick <tmck01@gmail.com>
2023-04-06 00:22:09 +1000

    update copilot installer

--- installers/linux/copilot.sh
+++ installers/linux/copilot.sh
@@ -2,18 +2,24 @@
 
 set -euo pipefail
 
-echo "- installing for vim"
-git clone https://github.com/github/copilot.vim.git \
-  ~/.vim/pack/github/start/copilot.vim
+if [ ! -d ~/.vim/pack/github/start/copilot.vim ]; then
+  echo "- installing for vim"
+  git clone https://github.com/github/copilot.vim.git \
+    ~/.vim/pack/github/start/copilot.vim
+fi
 
-echo "- installing for neovim"
-git clone https://github.com/github/copilot.vim.git \
-  ~/.config/nvim/pack/github/start/copilot.vim
+if [ ! -d ~/.config/nvim/pack/github/start/copilot.vim ]; then
+  echo "- installing for neovim"
+  git clone https://github.com/github/copilot.vim.git \
+    ~/.config/nvim/pack/github/start/copilot.vim
+fi
 
 echo "- installing for vscode"
 code --install-extension GitHub.copilot
 
-echo "! run :Copilot setup & :Copilot enable when in vim/nvim"
-
 echo "- installing nodejs"
-sudo apt install -y nodejs
+# nodejs needs to be version v16+, debian bullseye only ships with v12
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && \
+  sudo apt install -y nodejs
+
+echo "! run :Copilot setup & :Copilot enable when in vim/nvim"
