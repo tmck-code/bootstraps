835128882b261e56a35b89f0372da0626e300d47
Tom McKeesick <tmck01@gmail.com>
2024-02-14 22:06:34 +1100

    update fonts

--- installers/linux/cli-extras.sh
+++ installers/linux/cli-extras.sh
@@ -22,6 +22,16 @@ the_best=(
   'NScript.flf'
 )
 
+cd /tmp
+rm -rf figlet-fonts
+git clone git@github.com:xero/figlet-fonts.git
+
+sudo chown -R $USER:$USER /usr/share/figlet/
+mkdir -p /usr/share/figlet/fonts
+
+cp -v figlet-fonts/* /usr/share/figlet/fonts/
+rm -rf figlet-fonts
+
 mkdir -p "$HOME/bin/streaming/figlet/"
 for i in ${the_best[@]}; do
   ln -svf "/usr/share/figlet/fonts/$i" "$HOME/bin/streaming/figlet/"
